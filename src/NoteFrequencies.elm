1a151af36886b59f3a6a11532464281f487f6de9
Jakub Smetana <jakubsmetana@gmail.com>
2023-06-26 00:14:30 +0200

    Get correct list of frequencies

--- src/NoteFrequencies.elm
+++ src/NoteFrequencies.elm
@@ -1,5 +1,7 @@
 module NoteFrequencies exposing (main)
 
+import Array
+import Array exposing (Array)
 import Browser
 import Debug exposing (toString)
 import Html exposing (..)
@@ -26,22 +28,49 @@ frequencies : List Int
 frequencies =
     [ 436, 438, 440, 442, 444 ]
 
+octave : List String
+octave = 
+    [ "A", "A♯", "B", "C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯" ]
+
+subNumber : Int -> String
+subNumber number =
+    "0"
+
+
+getNote : Int -> String
+getNote halfsteps =
+    let
+        notes = 
+            if halfsteps < 0
+            then Array.fromList (List.reverse octave)
+            else Array.fromList octave
+        index = modBy 12 (abs halfsteps)
+        maybeNote =
+            Array.get index notes
+        note =
+            case maybeNote of
+                Just noteStr ->
+                    noteStr
+                Nothing ->
+                    "err"
+    in
+        note
 
 viewFrequencyToggle : Int -> Html Msg
 viewFrequencyToggle frequency =
     li [ onClick (ChangeBaseFrequency frequency) ] [ text (toString frequency) ]
 
 
-noteRow : String -> Int -> Html Msg
+noteRow : String -> Float -> Html Msg
 noteRow note frequency =
     tr []
         [ td [] [ text note ]
-        , td [] [ text (toString frequency ++ " Hz") ]
+        , td [] [ text (Round.round 2 frequency ++ " Hz") ]
         ]
 
 
 calculateFrequency : Int -> Int -> Float
-calculateFrequency halftones baseFrequency =
+calculateFrequency baseFrequency halftones =
     let
         constant = 2 ^ (1/12) -- the twelfth root of 2, because an octave has 12 half-steps and a tone doubles in frequency every octave in an equal tempered scale
     in
@@ -51,11 +80,33 @@ calculateFrequency halftones baseFrequency =
 tones : Int -> Html Msg
 tones baseFrequency =
     let
+        lowerThanBaseFreq =
+            List.range -47 -1
+            |> List.map (calculateFrequency baseFrequency)
+
+        higherThanBaseFreq =
+            List.range 1 47
+            |> List.map (calculateFrequency baseFrequency)
+
+        lowerThanBaseNotes =
+            List.range -47 -1
+            |> List.map getNote
+
+        higherThanBaseNotes =
+            List.range 1 47
+            |> List.map getNote
+
         lowerThanBase =
-            List.map (noteRow "l") (List.range -47 -1)
+            List.map2 noteRow lowerThanBaseNotes lowerThanBaseFreq
 
         higherThanBase =
-            List.map (noteRow "h") (List.range 1 47)
+            List.map2 noteRow higherThanBaseNotes higherThanBaseFreq
+
+        -- lowerThanBase =
+        --     List.map (noteRow "l") (List.range -47 -1)
+
+        -- higherThanBase =
+        --     List.map (noteRow "h") (List.range 1 47)
     in
     table [ id "frequencies" ]
         ([ tr []
@@ -64,7 +115,7 @@ tones baseFrequency =
             ]
          ]
             ++ lowerThanBase
-            ++ [ noteRow "A₄" baseFrequency ]
+            ++ [ noteRow "A₄" (toFloat baseFrequency) ]
             ++ higherThanBase
         )
 
